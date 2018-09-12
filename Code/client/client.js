c887409a7b23feeed03fb1a9907f2a7b9b947e6d
Patrick <u5583834@anu.edu.au>
2018-09-12 20:02:14 +1000

    Added code from earlier which allows taking and sending image with hardware button, then receiving and storing on server. Note: Client code currently needs to be run as root due to gpio ports not being pushed

--- Code/client/client.js
+++ Code/client/client.js
@@ -1,16 +1,36 @@
 const fs = require('file-system');
 const request = require('request');
 
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(203, 'in', 'rising', {debounceTimeout: 10});
+const exec = require('child_process').execSync;
+
 const destination_ip = 'http://192.168.0.100';
 const destination_port = ':3000';
 
-const formData = {
-    name: 'watermelon',
-    image: fs.createReadStream(__dirname + '/watermelon.png')
-};
+// Needs fswebcam installed to work
+// TODO Add npm dependencies
 
-request.post({url: destination_ip + destination_port + '/upload', formData: formData}, function (err, res, body) {
-    if (err) {return console.error('upload failed:', err);}
+// Needs to run as root while gpio is not permitted
 
-    console.log('Upload successful!  Server responded with:', body);
-});
+button.watch((err, value) => {
+    if (err) {throw err;}
+
+    console.log('Taking photo...');
+    exec('fswebcam -r 1920x1080 --no-banner image1.jpg');
+
+    const formData = {
+        name: 'image1',
+        image: fs.createReadStream(__dirname + '/image1.jpg')
+    };
+    
+    request.post({url: destination_ip + destination_port + '/upload', formData: formData}, function (err, res, body) {
+        if (err) {return console.error('upload failed:', err);}
+    
+        console.log('Upload successful!  Server responded with:', body);
+    });
+})
+
+process.on('SIGINT', () => {
+    button.unexport();
+  });
--- Code/server/server.js
+++ Code/server/server.js
@@ -1,6 +1,7 @@
 const express  = require("express");
 const multer  = require("multer");
 const app  = express();
+const ws = require('express-ws')(app);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
